5bb09da0801ad2a53f0c1f9b747c7ae4b6449c31
shad0wfir3 <shad0wfir3@crakgaming.co.za>
2015-03-28 19:34:54 +0200

    This is a test
    Just added a few words to edit.php under notes

--- application/view/note/edit.php
+++ application/view/note/edit.php
@@ -2,7 +2,7 @@
     <h1>NoteController/edit/:note_id</h1>
 
     <div class="box">
-        <h2>Edit a note</h2>
+        <h2>Edit a note this is a edit</h2>
 
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
@@ -13,6 +13,7 @@
                 <!-- we use htmlentities() here to prevent user input with " etc. break the HTML -->
                 <input type="hidden" name="note_id" value="<?php echo htmlentities($this->note->note_id); ?>" />
                 <input type="text" name="note_text" value="<?php echo htmlentities($this->note->note_text); ?>" />
+
                 <input type="submit" value='Change' />
             </form>
         <?php } else { ?>
